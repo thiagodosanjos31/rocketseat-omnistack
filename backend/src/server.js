0a613c209c5a067babb03ef63b156d42f2d5d8e1
Thiago dos Anjos <thiagoadosanjos@outlook.com.br>
2019-10-02 12:41:29 -0300

    Inicializando o backend

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+// GET, POST, PUT, DELETE
+
+
+app.get('/', (req, res) => {
+    return res.json({ message: "Hello Omnistack" });
+});
+
+app.listen(3333); //Porta que deve rodar a aplicação
